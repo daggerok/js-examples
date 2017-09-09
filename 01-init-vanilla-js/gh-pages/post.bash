8e5aae35c707e68a827a764d88ce42d6d78c1b91
Maksim Kostromin <daggerok@gmail.com>
2017-09-10 02:07:33 +0300

    Initial commit

---
+++ 01-init-vanilla-js/gh-pages/post.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# replace base-path with root:
+# /js-examples/ -> /
+
+if ! [ -z "$1" ]; then
+  sed -i -e "s/\<head\>\<base\ href=\"\/js-examples\/\"\/>/\<head\>/g" $1
+  sed -i -e "s/\(href=\"\/js-examples\/\)/href=\"\//g" $1
+  sed -i -e "s/\(src=\"\/js-examples\/\)/src=\"\//g" $1
+  rm -rf .nojekyll 404.html $-e
+fi
