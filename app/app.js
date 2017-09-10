6b2bca38614411398ced059a1fd078eac8f779d6
Maksim Kostromin <daggerok@gmail.com>
2017-09-10 05:11:54 +0300

    Github Pages 2017-09-10 deployment

--- app/app.js
+++ app/app.js
@@ -1,28 +1,10 @@
 $(function ready() {
 
-  function createTitle(text) {
-    var h1 = document.createElement('h1');
-    h1.textContent = text;
-    return h1;
-  }
-
-  var app = document.querySelector('#app');
-  var fragment = document.createDocumentFragment();
-  var ul = document.createElement('ul');
-
   $.getJSON('./app/data.json').then(function onData(data) {
-    data.map(function eachDataItem(item) {
-      var value = item.url + '/' + item.username;
-      var div = document.createElement('div');
-      var li = document.createElement('li');
-      div.textContent = value;
-      li.appendChild(div);
-      ul.appendChild(li);
-    });
-
-    fragment.appendChild(createTitle('Hey!'));
-    fragment.appendChild(ul);
-    app.appendChild(fragment);
+    var template = $('#data-template').html();
+    var users = { title: 'Hello!', users: data };
+    var html = Mustache.to_html(template, users);
+    $('#app').html(html);
   });
 
 });
