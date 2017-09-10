c2b3630e6bdcd5f5c0ae058b78d4aaaca5b9377e
Maksim Kostromin <daggerok@gmail.com>
2017-09-10 03:21:11 +0300

    fix sw

--- 01-init-vanilla-js/gh-pages/post.bash
+++ 01-init-vanilla-js/gh-pages/post.bash
@@ -7,5 +7,5 @@ if ! [ -z "$1" ]; then
   sed -i -e "s/\<head\>\<base\ href=\"\/js-examples\/\"\/>/\<head\>/g" $1
   sed -i -e "s/\(href=\"\/js-examples\/\)/href=\"\//g" $1
   sed -i -e "s/\(src=\"\/js-examples\/\)/src=\"\//g" $1
-  rm -rf .nojekyll 404.html $-e
+  rm -rf .nojekyll 404.html $1-e
 fi
