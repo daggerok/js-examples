4410295946e91aff7974396bac7ebc9443a9dbd2
Maksim Kostromin <daggerok@gmail.com>
2017-09-10 03:57:40 +0300

    jsonp

---
+++ 03-vanilla-js-jsonp/app/app.js
@@ -0,0 +1,24 @@
+function createTitle(text) {
+  var h1 = document.createElement('h1');
+  h1.textContent = text;
+  return h1;
+}
+
+var app = document.querySelector('#app');
+var fragment = document.createDocumentFragment();
+var ul = document.createElement('ul');
+
+function jsonpDataHandler(data) {
+  data.map(function eachDataItem(item) {
+    var value = item.url + '/' + item.username;
+    var div = document.createElement('div');
+    var li = document.createElement('li');
+    div.textContent = value;
+    li.appendChild(div);
+    ul.appendChild(li);
+  });
+
+  fragment.appendChild(createTitle('Hey!'));
+  fragment.appendChild(ul);
+  app.appendChild(fragment);
+}
