303d9e5dfba5a79798c6a59cc1907d3a461d3999
Maksim Kostromin <daggerok@gmail.com>
2017-09-10 03:57:55 +0300

    Github Pages 2017-09-10 deployment

--- app/app.js
+++ app/app.js
@@ -1,47 +1,24 @@
-(function app() {
+function createTitle(text) {
+  var h1 = document.createElement('h1');
+  h1.textContent = text;
+  return h1;
+}
 
-  function createTitle(text) {
-    var h1 = document.createElement('h1');
-    h1.textContent = text;
-    return h1;
-  }
+var app = document.querySelector('#app');
+var fragment = document.createDocumentFragment();
+var ul = document.createElement('ul');
 
-  function getRequest(toUrl) {
-    var request = !!window.XMLHttpRequest
-      ? new XMLHttpRequest()
-      : new ActiveXObject('Microsoft.XMLHTTP');
-    request.open('GET', toUrl);
-    request.send();
-    return request;
-  }
-
-  document.addEventListener('DOMContentLoaded', function bootstrap() {
-
-    var app = document.querySelector('#app');
-    var fragment = document.createDocumentFragment();
-    var ul = document.createElement('ul');
-
-    var request = getRequest('https://daggerok.github.io/js-examples/app/data.json');
-    request.addEventListener('readystatechange', function fetchData() {
-
-      if (request.readyState !== 4) return;
-      if (!request.responseText) return;
-
-      var data = JSON.parse(request.responseText);
-      data.map(function eachDataItem(item) {
-        var value = item.url + '/' + item.username;
-        var div = document.createElement('div');
-        var li = document.createElement('li');
-        div.textContent = value;
-        li.appendChild(div);
-        ul.appendChild(li);
-      });
-
-      fragment.appendChild(createTitle('Hey!'));
-      fragment.appendChild(ul);
-      app.appendChild(fragment);
-    }, false);
-
+function jsonpDataHandler(data) {
+  data.map(function eachDataItem(item) {
+    var value = item.url + '/' + item.username;
+    var div = document.createElement('div');
+    var li = document.createElement('li');
+    div.textContent = value;
+    li.appendChild(div);
+    ul.appendChild(li);
   });
 
-})();
+  fragment.appendChild(createTitle('Hey!'));
+  fragment.appendChild(ul);
+  app.appendChild(fragment);
+}
