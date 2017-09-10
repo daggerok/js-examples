a60493aeef9b3915494ed89148bee78404cccb5c
Maksim Kostromin <daggerok@gmail.com>
2017-09-10 05:58:13 +0300

    Github Pages 2017-09-10 deployment

--- app/app.js
+++ app/app.js
@@ -1,10 +1,25 @@
 $(function ready() {
 
+  function compile(selector, data) {
+    var template = $(selector).html();
+    return Mustache.to_html(template, data);
+  }
+
+  var title = compile('#title-template', { title: 'Hi!'});
+  $('#title').html(title);
+
   $.getJSON('./app/data.json').then(function onData(data) {
-    var template = $('#data-template').html();
-    var users = { title: 'Hello!', users: data };
-    var html = Mustache.to_html(template, users);
+    var html = compile('#data-template', { users: data });
     $('#app').html(html);
+    $('#app').cycle({
+      // fx: 'fade',
+      pause: 1,
+      next: '#next',
+      prev: '#prev',
+      speed: 500,
+      timeout: 5000,
+      opacity: 0.5,
+    });
   });
 
 });
