1807001760842963054354179967969ddf752f8e
Maksim Kostromin <daggerok@gmail.com>
2017-09-18 05:38:54 +0300

    Github Pages 2017-09-18 deployment

--- app/app.js
+++ app/app.js
@@ -1,49 +1,62 @@
 (function app() {
 
-  /**
-   * ES6
-   */
-  class ES6User {
-    constructor(username, password, email) {
-      this.username = username;
-      this.password = password;
-      this.email = email;
+  class EventObserver {
+    constructor() {
+      this.subscribers = [];
+    }
+
+    // add listener
+    subscribe(subscriber) {
+      this.subscribers = [
+        ...this.subscribers,
+        subscriber,
+      ];
+      return () => this.unSubscribe(subscriber);
+    }
+
+    // remove listener
+    unSubscribe(subscriber) {
+      this.subscribers = this.subscribers.filter(
+        s => s !== subscriber
+      );
+    }
+
+    // fire event
+    broadcast(data) {
+      this.subscribers.forEach(s => s(data));
     }
   }
 
-  /**
-   * ES5
-   */
-  function ES5User(username, password, email) {
-    this.username = username;
-    this.password = password;
-    this.email = email;
-  }
-
-  const stringMe = obj => JSON.stringify(obj, null, 2);
-  const pre = content => `<pre>${content}</pre>`;
-
   document.addEventListener('DOMContentLoaded', function bootstrap() {
 
-    const app = document.querySelector('#app');
-    const fragment = document.createDocumentFragment();
-    const render = innerHTML => {
+    // create listener
+    const observer = new EventObserver();
+
+    // send data
+    document.querySelector('input[type="text"]').addEventListener('keyup', e => {
+      e.preventDefault();
+      const { keyCode, target } = e;
+      if (!keyCode || keyCode !== 13) return;
+      if (!target || !target.value|| !target.value.trim().length) return;
+      observer.broadcast({
+        message: target.value.trim(),
+        time: new Date().toLocaleTimeString()
+      });
+      target.value = '';
+    }, false);
+
+    // receive data
+    const messages = document.querySelector('#messages');
+
+    const render = envelope => {
       const el = document.createElement('div');
-      el.innerHTML = innerHTML;
-      fragment.appendChild(el);
+      el.innerHTML = `<div>
+        ${envelope.time}: ${envelope.message}
+      </div>`;
+      messages.prepend(el);
     };
 
-    const es6User = new ES6User('Max', 'es6', 'daggerok@gmail.com');
-    const es5User = new ES5User('Max', 'es5', 'daggerok@gmail.com');
-
-    render(`<h2>Constructor</h2>`);
-
-    [es6User, es5User,]
-      .map(stringMe)
-      .map(pre)
-      .forEach(render);
-
-    app.appendChild(fragment);
+    observer.subscribe(render);
 
   });
 
