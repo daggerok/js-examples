a0f7fed752ec2ea9b69e01e385655d391f0aa3b2
Maksim Kostromin <daggerok@gmail.com>
2018-07-24 01:58:45 +0300

    DEAL WITH IT (C) whatthecommit.com

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # functional-js
 for path in \
-  ramda-functional-composition-app \
-  ramda-currying-app \
+  ramda-functional-composition \
+  ramda-currying \
   high-order-functions \
-  starter-app \
+  starter \
 ; do
   rm -rf functional-js/$path/dist
   rm -rf functional-js/$path/.idea
