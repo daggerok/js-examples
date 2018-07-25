0bad020844336d1007a0f92089f4fd747558f664
Maksim Kostromin <daggerok@gmail.com>
2018-07-26 02:37:48 +0300

    herpderp (redux) (C) whatthecommit.com

--- functional-js/functional-temperature-converter-app/src/app/Views.js
+++ functional-js/functional-temperature-converter-app/src/app/Views.js
@@ -6,8 +6,24 @@ const { div, h1, pre, } = hh(h);
 export function view(dispatch, state) {
   return contentWrapper([
     applicationHeader(),
+    div({ className: '' }, [
+      celsiusComponent(dispatch, state),
+      fahrenheitComponent(dispatch, state),
+    ]),
     debugAppState(state),
   ]);
+}
+
+function debugAppState(state) {
+  return pre(JSON.stringify(state, null, 2));
+}
+
+function fahrenheitComponent(dispatch, state) {
+  return div({ className: 'f2 pv2' }, 'TODO: fahrenheit');
+}
+
+function celsiusComponent(dispatch, state) {
+  return div({ className: 'f2 pv2' }, 'TODO: celsius');
 }
 
 function applicationHeader() {
@@ -18,8 +34,4 @@ function contentWrapper(elements) {
   return div({ className: 'mw6 center' }, [
     ...elements,
   ]);
-}
-
-function debugAppState(state) {
-  return pre(JSON.stringify(state, null, 2));
 }
