b6684a3fdb253bcebb75000210c32300a50c52ee
Maksim Kostromin <daggerok@gmail.com>
2018-08-23 16:40:39 +0300

    Github Pages 2018-08-23 deployment

--- app/app.js
+++ app/app.js
@@ -1,28 +1,68 @@
-$(document).ready(function onDocumentReady() {
+(function app() {
 
-  function createTitle(text) {
-    var h1 = document.createElement('h1');
-    h1.textContent = text;
-    return h1;
+  class SuperComplexLogic1 {
+    calculate(_skip) {
+      // super complex logic here... we don't want to deep dive into it
+      return Date.now() % 1000 < 500;
+    }
   }
 
-  var app = document.querySelector('#app');
-  var fragment = document.createDocumentFragment();
-  var ul = document.createElement('ul');
+  class VeryComplexLogic2 {
+    calculate(_skip) { // same
+      return Date.now() % 9 > 5;
+    }
+  }
 
-  $.getJSON('./app/data.json').then(function onData(data) {
-    data.map(function eachDataItem(item) {
-      var value = item.url + '/' + item.username;
-      var div = document.createElement('div');
-      var li = document.createElement('li');
-      div.textContent = value;
-      li.appendChild(div);
-      ul.appendChild(li);
-    });
+  class AcademicLogic3 {
+    calculate(_skip) { // same
+      return Date.now() % 2 === 0;
+    }
+  }
 
-    fragment.appendChild(createTitle('Hey!'));
-    fragment.appendChild(ul);
+  class Facade {
+    constructor(name) {
+      this.name = name;
+    }
+
+    /**
+     * API method (to reduce complexity)
+     * @param value
+     * @returns {string}
+     */
+    html(_whatever) {
+      const result1 = new SuperComplexLogic1().calculate(_whatever);
+      const result2 = new VeryComplexLogic2().calculate({_whatever});
+      const result3 = new AcademicLogic3().calculate([_whatever]);
+      return `
+      <div>
+        <p>${this.name} results:</p>
+        <ul>
+          <li>${result1}</li>
+          <li>${result2}</li>
+          <li>${result3}</li>
+        </ul>
+      </div>
+      `;
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function bootstrap() {
+
+    const app = document.querySelector('#app');
+    const fragment = document.createDocumentFragment();
+    const addTo = innerHTML => {
+      const el = document.createElement('div');
+      el.innerHTML = innerHTML;
+      fragment.appendChild(el);
+    };
+
+    ['Max', 'Bob', 'Nobody']
+      .map(name => new Facade(name))
+      .map(facade => facade.html())
+      .forEach(addTo);
+
     app.appendChild(fragment);
+
   });
 
-});
+})();
