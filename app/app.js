8a35dbe673dc764d7e5d8077e3466b1a3a410e3c
Maksim Kostromin <daggerok@gmail.com>
2018-08-23 16:51:04 +0300

    Github Pages 2018-08-23 deployment

--- app/app.js
+++ app/app.js
@@ -1,79 +1,25 @@
-(function app() {
+$(function ready() {
 
-  class Coffee {
-    cost() {
-      return 5;
-    }
+  function compile(selector, data) {
+    var template = $(selector).html();
+    return Mustache.to_html(template, data);
   }
 
-  const withSugar = coffee => {
-    const cost = coffee.cost();
-    return {
-      ...coffee,
-      cost: () => cost + 0.5,
-    }
-  };
+  var title = compile('#title-template', { title: 'Hi!'});
+  $('#title').html(title);
 
-  const withMilk = coffee => {
-    const cost = coffee.cost();
-    return {
-      ...coffee,
-      cost: () => cost + 1,
-    }
-  };
-
-  const small = coffee => {
-    const cost = coffee.cost();
-    return {
-      ...coffee,
-      cost: () => cost - 1,
-    }
-  };
-
-  const double = coffee => {
-    const cost = coffee.cost();
-    return {
-      ...coffee,
-      cost: () => cost + 1,
-    }
-  };
-
-  document.addEventListener('DOMContentLoaded', function bootstrap() {
-
-    const app = document.querySelector('#app');
-    const fragment = document.createDocumentFragment();
-    const render = innerHTML => {
-      const el = document.createElement('div');
-      el.innerHTML = innerHTML;
-      fragment.appendChild(el);
-    };
-
-    const coffee = new Coffee();
-
-    const menu = {
-      'small coffee': small(coffee),
-      'small coffee with sugar': withSugar(small(coffee)),
-      'small coffee with milk': withMilk(small(coffee)),
-      'small coffee with sugar and milk': withSugar(withMilk(small(coffee))),
-      'coffee': coffee,
-      'coffee with sugar': withSugar(coffee),
-      'coffee with milk': withMilk(coffee),
-      'coffee with sugar and milk': withSugar(withMilk(coffee)),
-      'double coffee': double(coffee),
-      'double coffee with sugar': withSugar(double(coffee)),
-      'double coffee with milk': withMilk(double(coffee)),
-      'double coffee with sugar and milk': withSugar(withMilk(double(coffee))),
-    };
-
-    render(`<h2>Decorators</h2>`);
-    Object.keys(menu).forEach(key => render(`
-      <div>
-        ${key}: ${menu[key].cost()}
-      </div>`
-    ));
-
-    app.appendChild(fragment);
-
+  $.getJSON('./app/data.json').then(function onData(data) {
+    var html = compile('#data-template', { users: data });
+    $('#app').html(html);
+    $('#app').cycle({
+      // fx: 'fade',
+      pause: 1,
+      next: '#next',
+      prev: '#prev',
+      speed: 500,
+      timeout: 5000,
+      opacity: 0.5,
+    });
   });
 
-})();
+});
