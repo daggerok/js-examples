90d4eece163c55ac39323d4564cf2ae96458cde2
Maksim Kostromin <daggerok@gmail.com>
2018-08-23 16:37:28 +0300

    Github Pages 2018-08-23 deployment

--- app/app.js
+++ app/app.js
@@ -1,24 +1,28 @@
-function createTitle(text) {
-  var h1 = document.createElement('h1');
-  h1.textContent = text;
-  return h1;
-}
+$(document).ready(function onDocumentReady() {
 
-var app = document.querySelector('#app');
-var fragment = document.createDocumentFragment();
-var ul = document.createElement('ul');
+  function createTitle(text) {
+    var h1 = document.createElement('h1');
+    h1.textContent = text;
+    return h1;
+  }
 
-function jsonpDataHandler(data) {
-  data.map(function eachDataItem(item) {
-    var value = item.url + '/' + item.username;
-    var div = document.createElement('div');
-    var li = document.createElement('li');
-    div.textContent = value;
-    li.appendChild(div);
-    ul.appendChild(li);
+  var app = document.querySelector('#app');
+  var fragment = document.createDocumentFragment();
+  var ul = document.createElement('ul');
+
+  $.getJSON('./app/data.json').then(function onData(data) {
+    data.map(function eachDataItem(item) {
+      var value = item.url + '/' + item.username;
+      var div = document.createElement('div');
+      var li = document.createElement('li');
+      div.textContent = value;
+      li.appendChild(div);
+      ul.appendChild(li);
+    });
+
+    fragment.appendChild(createTitle('Hey!'));
+    fragment.appendChild(ul);
+    app.appendChild(fragment);
   });
 
-  fragment.appendChild(createTitle('Hey!'));
-  fragment.appendChild(ul);
-  app.appendChild(fragment);
-}
+});
