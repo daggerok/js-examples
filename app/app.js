00ff8de1608d61afc5711363e5a72e18280aef61
Maksim Kostromin <daggerok@gmail.com>
2018-08-23 16:35:22 +0300

    Github Pages 2018-08-23 deployment

--- app/app.js
+++ app/app.js
@@ -1,10 +1,24 @@
-(function app() {
+function createTitle(text) {
+  var h1 = document.createElement('h1');
+  h1.textContent = text;
+  return h1;
+}
 
-  document.addEventListener('DOMContentLoaded', function bootstrap() {
-    console.log('2');
-  }, false);
+var app = document.querySelector('#app');
+var fragment = document.createDocumentFragment();
+var ul = document.createElement('ul');
 
-  console.log('1');
+function jsonpDataHandler(data) {
+  data.map(function eachDataItem(item) {
+    var value = item.url + '/' + item.username;
+    var div = document.createElement('div');
+    var li = document.createElement('li');
+    div.textContent = value;
+    li.appendChild(div);
+    ul.appendChild(li);
+  });
 
-})();
-  
+  fragment.appendChild(createTitle('Hey!'));
+  fragment.appendChild(ul);
+  app.appendChild(fragment);
+}
