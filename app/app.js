cc9d5a0024ac5b2a4352b4b301f3bfd04079ef3b
Maksim Kostromin <daggerok@gmail.com>
2018-08-23 16:48:53 +0300

    Github Pages 2018-08-23 deployment

--- app/app.js
+++ app/app.js
@@ -1,30 +1,77 @@
 (function app() {
 
-  function createTitle(text) {
-    const h1 = document.createElement('h1');
-    h1.textContent = text;
-    return h1;
+  class Coffee {
+    cost() {
+      return 5;
+    }
   }
+
+  const withSugar = coffee => {
+    const cost = coffee.cost();
+    return {
+      ...coffee,
+      cost: () => cost + 0.5,
+    }
+  };
+
+  const withMilk = coffee => {
+    const cost = coffee.cost();
+    return {
+      ...coffee,
+      cost: () => cost + 1,
+    }
+  };
+
+  const small = coffee => {
+    const cost = coffee.cost();
+    return {
+      ...coffee,
+      cost: () => cost - 1,
+    }
+  };
+
+  const double = coffee => {
+    const cost = coffee.cost();
+    return {
+      ...coffee,
+      cost: () => cost + 1,
+    }
+  };
 
   document.addEventListener('DOMContentLoaded', function bootstrap() {
 
     const app = document.querySelector('#app');
     const fragment = document.createDocumentFragment();
-    const ul = document.createElement('ul');
+    const render = innerHTML => {
+      const el = document.createElement('div');
+      el.innerHTML = innerHTML;
+      fragment.appendChild(el);
+    };
 
-    fetch('/app/data.json')
-      .then(data => data.json())
-      .then(items => items.map(item => {
-        const value = item.url + '/' + item.username;
-        const div = document.createElement('div');
-        const li = document.createElement('li');
-        div.textContent = value;
-        li.appendChild(div);
-        ul.appendChild(li);
-      }));
+    const coffee = new Coffee();
 
-    fragment.appendChild(createTitle('Hey!'));
-    fragment.appendChild(ul);
+    const menu = {
+      'small coffee': small(coffee),
+      'small coffee with sugar': withSugar(small(coffee)),
+      'small coffee with milk': withMilk(small(coffee)),
+      'small coffee with sugar and milk': withSugar(withMilk(small(coffee))),
+      'coffee': coffee,
+      'coffee with sugar': withSugar(coffee),
+      'coffee with milk': withMilk(coffee),
+      'coffee with sugar and milk': withSugar(withMilk(coffee)),
+      'double coffee': double(coffee),
+      'double coffee with sugar': withSugar(double(coffee)),
+      'double coffee with milk': withMilk(double(coffee)),
+      'double coffee with sugar and milk': withSugar(withMilk(double(coffee))),
+    };
+
+    render(`<h2>Decorators</h2>`);
+    Object.keys(menu).forEach(key => render(`
+      <div>
+        ${key}: ${menu[key].cost()}
+      </div>`
+    ));
+
     app.appendChild(fragment);
 
   });
