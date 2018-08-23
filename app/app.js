31c0cc5d9957481156ef65532dd0b12b743b9d9b
Maksim Kostromin <daggerok@gmail.com>
2018-08-23 16:47:21 +0300

    Github Pages 2018-08-23 deployment

--- app/app.js
+++ app/app.js
@@ -1,55 +1,30 @@
 (function app() {
 
-  const logger = (strategy, level, ...rest) =>
-    strategy(level, ...rest/* messages, etc...*/);
-
-  const logToConsoleStrategy = (target, level, ...messages) =>
-    target[level](...messages);
-
-  const styles = {
-    log: 'primary',
-    info: 'info',
-    warn: 'warning',
-    error: 'danger',
-  };
-
-  const logToDomStrategy = (target, level, ...messages) =>
-    target.innerHTML = messages.map(message => `
-      <div class="badge badge-${styles[level]}">
-        ${message}
-      </div>
-    `).join(`<br/>`);
+  function createTitle(text) {
+    const h1 = document.createElement('h1');
+    h1.textContent = text;
+    return h1;
+  }
 
   document.addEventListener('DOMContentLoaded', function bootstrap() {
 
-    // 1) console logger:
-    logger(
-      logToConsoleStrategy,
-      console,
-      'warn',
-      'console warn message 1',
-      'and console warn message 2',
-    );
-
     const app = document.querySelector('#app');
     const fragment = document.createDocumentFragment();
+    const ul = document.createElement('ul');
 
-    const p = document.createElement('p');
-    p.textContent = 'also check browser console';
-    fragment.appendChild(p);
+    fetch('/app/data.json')
+      .then(data => data.json())
+      .then(items => items.map(item => {
+        const value = item.url + '/' + item.username;
+        const div = document.createElement('div');
+        const li = document.createElement('li');
+        div.textContent = value;
+        li.appendChild(div);
+        ul.appendChild(li);
+      }));
 
-    const div = document.createElement('div');
-
-    logger(
-      logToDomStrategy,
-      div,
-      'warn',
-      'DOM warn message 1',
-      'DOM warn message 2',
-      'DOM warn message 3...',
-    );
-
-    fragment.appendChild(div);
+    fragment.appendChild(createTitle('Hey!'));
+    fragment.appendChild(ul);
     app.appendChild(fragment);
 
   });
